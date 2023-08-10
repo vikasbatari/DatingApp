7abc6683787a0eca166e253faf1cb754cbd51454
Vikas Sharma (Contractor) <Vikas.Sharma@paccar.com>
2023-08-10 13:56:13 +0530

    Intial commit

---
+++ DatingApp/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpGet]
+        [Route("id")]
+        public ActionResult<AppUser> GetUsers(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
